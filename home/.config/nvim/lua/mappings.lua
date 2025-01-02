6e185d9682283add70e6c900910533d3eb04842c
Dzejkop <jakubtrad@gmail.com>
2025-01-02 10:32:27 +0100

    (nvim) Add mapping to telescope references

--- home/.config/nvim/lua/mappings.lua
+++ home/.config/nvim/lua/mappings.lua
@@ -8,5 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 map("n", "<leader>fs", "<cmd> Telescope lsp_document_symbols <cr>", { desc = "telescope document symbols" })
 map("n", "<leader>fS", "<cmd> Telescope lsp_workspace_symbols <cr>", { desc = "telescope workspace symbols" })
+map("n", "<leader>fr", "<cmd> Telescope lsp_references <cr>", { desc = "telescope references" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
