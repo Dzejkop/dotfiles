f526c427b19cbd270f59e073a2d3187dd3fbe444
Dzejkop <jakubtrad@gmail.com>
2025-01-06 09:29:58 +0100

    add grug far

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -81,4 +81,15 @@ return {
       require("cmp-npm").setup {}
     end,
   },
+  {
+    "MagicDuck/grug-far.nvim",
+    lazy = false,
+    config = function()
+      require("grug-far").setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      }
+    end,
+  },
 }
