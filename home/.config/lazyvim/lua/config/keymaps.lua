4601c9016e291ba2caadc4334c6bf97bb8b3144e
Dzejkop <jakubtrad@gmail.com>
2025-03-05 19:59:00 +0100

    remove dumb keymap

--- home/.config/lazyvim/lua/config/keymaps.lua
+++ home/.config/lazyvim/lua/config/keymaps.lua
@@ -2,5 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-j>", "<C-e>j")
-vim.keymap.set("n", "<C-k>", "<C-y>k")
