a0832fe08ae96ba929a47cc07047ca8d29df3940
Dzejkop <jakubtrad@gmail.com>
2025-03-16 21:25:56 +0100

    Toggle tree-sitter-context

--- home/.config/lazyvim/lua/config/keymaps.lua
+++ home/.config/lazyvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>tt", "<cmd>TSContextToggle<cr>", { desc = "Toggle tree-sitter context" })
