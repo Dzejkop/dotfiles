4dc324047153b71a3aa13b6f26adb5130b9e852a
Dzejkop <jakubtrad@gmail.com>
2025-05-07 13:01:38 +0200

    fix mason issue

---
+++ home/.config/lazyvim/lua/plugins/mason.lua
@@ -0,0 +1,5 @@
+-- Can be removed once https://github.com/LazyVim/LazyVim/issues/6039 is resolved
+return {
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+}
