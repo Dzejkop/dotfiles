c7048435065acd403b0d822d6b4e429b31ffa4fa
Dzejkop <jakubtrad@gmail.com>
2025-05-14 12:59:19 +0200

    disable the proc-macro-disabled RA diagnostic

--- home/.config/lazyvim/lua/config/options.lua
+++ home/.config/lazyvim/lua/config/options.lua
@@ -2,3 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.g.snacks_animate = false
+
+vim.g.rustaceanvim = {
+  server = {
+    default_settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          enable = true,
+          disabled = { "proc-macro-disabled" },
+        },
+      },
+    },
+  },
+}
